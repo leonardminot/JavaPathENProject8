db40e8b4f1887480d356d43a78e9477ec0ec5c7f
leonard.minot <leonard.minot@gadz.org>
2024-03-28 17:32:02 +0100

    RewardsService::calculateRewardsForUser: use of parallelStream to increase intrinsic speed

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -19,72 +19,77 @@ import com.openclassrooms.tourguide.user.UserReward;
 public class RewardsService {
     private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
 
-	// proximity in miles
+    // proximity in miles
     private final int defaultProximityBuffer = 10;
-	private int proximityBuffer = defaultProximityBuffer;
-	private final int attractionProximityRange = 200;
-	private final GpsUtil gpsUtil;
-	private final RewardCentral rewardsCentral;
+    private int proximityBuffer = defaultProximityBuffer;
+    private final int attractionProximityRange = 200;
+    private final GpsUtil gpsUtil;
+    private final RewardCentral rewardsCentral;
 
-	private final ExecutorService executor = Executors.newFixedThreadPool(100);
-	
-	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
-		this.gpsUtil = gpsUtil;
-		this.rewardsCentral = rewardCentral;
-	}
-	
-	public void setProximityBuffer(int proximityBuffer) {
-		this.proximityBuffer = proximityBuffer;
-	}
-	
-	public void setDefaultProximityBuffer() {
-		proximityBuffer = defaultProximityBuffer;
-	}
+    private final ExecutorService executor = Executors.newFixedThreadPool(100);
 
-	public CompletableFuture<Void> calculateRewards(User user) {
-		return CompletableFuture.runAsync(() -> {
-			calculateRewardsForUser(user);
+    public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
+        this.gpsUtil = gpsUtil;
+        this.rewardsCentral = rewardCentral;
+    }
+
+    public void setProximityBuffer(int proximityBuffer) {
+        this.proximityBuffer = proximityBuffer;
+    }
+
+    public void setDefaultProximityBuffer() {
+        proximityBuffer = defaultProximityBuffer;
+    }
+
+    public CompletableFuture<Void> calculateRewards(User user) {
+        return CompletableFuture.runAsync(() -> {
+            calculateRewardsForUser(user);
         }, executor);
-	}
+    }
 
-	public void calculateRewardsForUser(User user) {
-		List<VisitedLocation> userLocations = user.getVisitedLocations();
-		List<Attraction> attractions = gpsUtil.getAttractions();
-		
-		for(VisitedLocation visitedLocation : userLocations) {
-			for(Attraction attraction : attractions) {
-				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
-					if(nearAttraction(visitedLocation, attraction)) {
-						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-					}
-				}
-			}
-		}
-	}
+    public void calculateRewardsForUser(User user) {
 
-	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
-		return !(getDistance(attraction, location) > attractionProximityRange);
-	}
-	
-	private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
-		return !(getDistance(attraction, visitedLocation.location) > proximityBuffer);
-	}
-	
-	public int getRewardPoints(Attraction attraction, User user) {
-		return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
-	}
-	
-	public double getDistance(Location loc1, Location loc2) {
+        List<VisitedLocation> userLocations = user.getVisitedLocations();
+        List<Attraction> attractions = gpsUtil.getAttractions();
+
+        // Utilisation de // stream pour augmenter la vitesse intrinsèque de l'algorithme
+        userLocations.parallelStream()
+                .forEach(visitedLocation -> attractions.parallelStream()
+                        .forEach(attraction -> {
+                            if (hasUserGotNoRewardForGivenAttraction(user, attraction))
+                                if (nearAttraction(visitedLocation, attraction))
+                                    user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+                        }));
+    }
+
+    private boolean hasUserGotNoRewardForGivenAttraction(User user, Attraction attraction) {
+        return user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName));
+    }
+
+
+    public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
+        return !(getDistance(attraction, location) > attractionProximityRange);
+    }
+
+    private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
+        return !(getDistance(attraction, visitedLocation.location) > proximityBuffer);
+    }
+
+    public int getRewardPoints(Attraction attraction, User user) {
+        return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
+    }
+
+    public double getDistance(Location loc1, Location loc2) {
         double lat1 = Math.toRadians(loc1.latitude);
         double lon1 = Math.toRadians(loc1.longitude);
         double lat2 = Math.toRadians(loc2.latitude);
         double lon2 = Math.toRadians(loc2.longitude);
 
         double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
-                               + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+                + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
 
         double nauticalMiles = 60 * Math.toDegrees(angle);
         return STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-	}
+    }
 
 }
